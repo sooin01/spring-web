c43786210f62d54cc8177576178d2abbcb9e1417
kdw <kdw@kdw-PC>
2016-02-20 13:37:02 +0900

    add

--- src/main/java/com/my/app/sample/dto/RequestParamSampleDto.java
+++ src/main/java/com/my/app/sample/dto/RequestParamSampleDto.java
@@ -1,13 +1,17 @@
 package com.my.app.sample.dto;
 
+import com.my.app.common.annotation.ParamName;
 import com.my.app.common.dto.BaseDto;
 
 public class RequestParamSampleDto extends BaseDto {
 
 	private static final long serialVersionUID = -3979742997193753008L;
-	
+
+	@ParamName("user_id")
+	private String userId;
+	@ParamName("user_name")
+	private String userName;
 	private String user_id;
-	private String user_name;
 
 	public String getUser_id() {
 		return user_id;
@@ -23,6 +27,24 @@ public class RequestParamSampleDto extends BaseDto {
 
 	public void setUser_name(String user_name) {
 		this.user_name = user_name;
+	}
+
+	private String user_name;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 
 }
