16e619ef729d36d43dd7dec76d309fde9befda1c
kdw <kdw@kdw-PC>
2016-02-22 02:10:45 +0900

    add

--- src/main/java/com/my/app/common/domain/BaseDomain.java
+++ src/main/java/com/my/app/common/domain/BaseDomain.java
@@ -2,6 +2,8 @@ package com.my.app.common.domain;
 
 import java.io.Serializable;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
@@ -9,6 +11,15 @@ public class BaseDomain implements Serializable {
 
 	private static final long serialVersionUID = -2858165339117735946L;
 	
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj, true);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this, true);
+	}
 	@Override
 	public String toString() {
 		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
--- src/test/java/com/my/app/user/dao/UserDaoTest.java
+++ src/test/java/com/my/app/user/dao/UserDaoTest.java
@@ -5,9 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"/spring/*-context.xml"})
+@WebAppConfiguration
 public class UserDaoTest {
 
 	@Autowired
