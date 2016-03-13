8a6fe67de3903c9eafb541ad576782f4edd8d6a1
kdw <kdw@kdw-PC>
2016-03-14 01:00:53 +0900

    add

--- src/main/java/com/my/app/contact/controller/ContactController.java
+++ src/main/java/com/my/app/contact/controller/ContactController.java
@@ -3,8 +3,11 @@ package com.my.app.contact.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.my.app.contact.domain.Contact;
@@ -15,9 +18,14 @@ public class ContactController {
 	
 	@Autowired
 	private ContactService contactService;
+	
+	@Autowired
+	private MessageSourceAccessor messageSourceAccessor;
 
 	@RequestMapping(value = "/contact", method = RequestMethod.GET, produces = "application/json")
+	@ResponseStatus(code = HttpStatus.OK)
 	public List<Contact> getContactList() {
+		System.out.println(messageSourceAccessor.getMessage("welcome"));
 		return contactService.getContactList();
 	}
 	
